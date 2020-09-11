3d39303f4ae40a8b40969b773ab21f6101881cfa
Aissa Nouacer <aissanouacer@gmail.com>
2020-09-11 13:46:22 -0400

    adding some comments

--- base/main.py
+++ base/main.py
@@ -9,6 +9,11 @@ from connected import Connected
 
 class Login(Screen):
     def do_login(self, usr_text, pass_text):
+        '''
+        do_login it's on_press botton to login fired, params taken from ids of the
+        TextInput, basic controll of the fields must be filled with popup notification
+        when all fields Ok, check the user and password if they are valid.
+        '''
         app = App.get_running_app()
 
         app.username = usr_text
@@ -45,6 +50,9 @@ class Login(Screen):
         #  app.config.read(app.get_application_config())
         #  app.config.write()
     def reset_form(self):
+        '''
+        reset_form cleaning the fields by calling it with self.ids[].text=""
+        '''
         self.ids['usr'].text = ""
         self.ids['password'].text = ""
 
@@ -52,23 +60,15 @@ class Login(Screen):
 
 
 class LoginApp(App):
-    username = StringProperty(None)
-    password = StringProperty(None)
-
+    '''
+    main class, it has ScreenManager with two widgets (login and connected widgets)
+    and returning that manager
+    '''
     def build(self):
         manager = ScreenManager()
 
         manager.add_widget(Login(name='login'))
         manager.add_widget(Connected(name='connected'))
         return manager
-    #  def get_application_config(self):
-        #  if(not self.username):
-            #  return super(LoginApp, self).get_application_config()
-        #  conf_directory = os.path.join(self.user_data_dir ,self.username)
-        #  if( not os.path.exists(conf_directory)):
-            #  os.makedirs(conf_directory)
-            #  return super(LoginApp,self).get_application_config(
-                #  '{}/config.cfg'.format(conf_directory))
-
 if __name__ == "__main__":
     LoginApp().run()
